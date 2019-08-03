5d11d44d2e06ec5f4db934dc412fff93ec8582b2
Paulo Ricardo Koch <pkoch@paulos-mbp.localdomain>
2019-08-03 00:46:26 -0300

    delete .idea

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import image from '../../../assets/images/burger-logo.png';
+
+const logo = props => (
+    <div>
+        <img src={image} alt="logo"/>
+    </div>
+);
+
+export default logo;
